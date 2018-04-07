f0a587808ca444bfd8748ccdb8ad78c12cf976bb
Daniel Gill <dwgill@outlook.com>
2018-04-07 09:58:17 -0400

    Requirement reducers

--- src/reducers/requirements.js
+++ src/reducers/requirements.js
@@ -1,3 +1,4 @@
+import pullAt from 'lodash/fp/pullAt';
 import {
   ADD_REQUIREMENT,
   REMOVE_REQUIREMENT,
@@ -6,6 +7,21 @@ import {
 
 const initialState = [];
 
-const requirementsReducer = (state = initialState, action) => {};
+const requirementsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_REQUIREMENT: {
+      const newReq = action.payload;
+      return state.concat(newReq);
+    }
+    case REMOVE_REQUIREMENT: {
+      const indexToRemove = action.payload;
+      return pullAt(indexToRemove)(state);
+    }
+    case REPLACE_REQUIREMENTS: {
+      const newReqs = action.payload;
+      return newReqs || initialState;
+    }
+  }
+};
 
 export default requirementsReducer;
