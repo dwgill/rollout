19bf7c51b6c8e083d3fc3cfe36cad8b1f76afb8b
Daniel Gill <dwgill@outlook.com>
2018-04-22 08:24:21 -0400

    Fix presets modal on mobile by removing flex

--- src/components/RequirementDisplay/PresetsModal/PresetsModalInterior/PresetsModalInteriorView.js
+++ src/components/RequirementDisplay/PresetsModal/PresetsModalInterior/PresetsModalInteriorView.js
@@ -24,56 +24,49 @@ const A = ({ link, children }) => (
   </a>
 );
 
+const Button = ({ title, children, onClick }) => (
+  <li className={styles.button} onClick={onClick} role="button">
+    <h3 className={styles.buttonTitle}>{title}</h3>
+    <p className={styles.buttonBody}>{children}</p>
+  </li>
+);
+
 const PresetsModalInteriorView = ({
   onSetColville: handleSetColville,
   onSetColvilleClassic: handleSetColvilleClassic,
   onSetMercer: handleSetMercer,
   onSetMercerPlus: handleSetMercerPlus,
 }) => (
-  <>
+  <div className={styles.container}>
     <h2 className={styles.title}>Preset Requirements</h2>
-    <div className={styles.item} onClick={handleSetColville} role="button">
-      <h3 className={styles.itemTitle}>The Colville</h3>
-      <p className={styles.itemBody}>
+    <ul className={styles.buttons}>
+      <Button title="The Colville" onClick={handleSetColville}>
         A method described by Matt Colville in his{' '}
         <A link={colvilleLink}>Running the Game</A> series. Roll the standard
         4d6, drop 1, six times in order, but start over again if the all the
         modifiers add up to less than +2.
-      </p>
-    </div>
-    <div
-      className={styles.item}
-      onClick={handleSetColvilleClassic}
-      role="button"
-    >
-      <h3 className={styles.itemTitle}>The Colville Classic</h3>
-      <p className={styles.itemBody}>
+      </Button>
+      <Button title="The Colville Classic" onClick={handleSetColvilleClassic}>
         The original approach{' '}
         <A link={colvilleClassicLink}>advocated by Matt Colville</A> when
         introducing players to Dungeons & Dragons. Roll 4d6, drop 1, in order,
         but discard the rollout if there aren't at least two scores 15 or
         higher.
-      </p>
-    </div>
-    <div className={styles.item} onClick={handleSetMercer} role="button">
-      <h3 className={styles.itemTitle}>The Mercer</h3>
-      <p className={styles.itemBody}>
+      </Button>
+      <Button title="The Mercer" onClick={handleSetMercer}>
         An approach <A link={mercerLink}>used by Matt Mercer</A> of Critical
         Role fame. Roll your stats using the standard 4d6 drop 1, but roll again
         if the attributes don't add up to 70 or more.
-      </p>
-    </div>
-    <div className={styles.item} onClick={handleSetMercerPlus} role="button">
-      <h3 className={styles.itemTitle}>The Mercer+</h3>
-      <p className={styles.itemBody}>
+      </Button>
+      <Button title="The Mercer+" onClick={handleSetMercerPlus}>
         <A link={mercerLink}>
-          Same approach by Matt Mercer, but with a higher threshold.
+          The same approach by Matt Mercer, but with a higher threshold.
         </A>{' '}
         Roll stats as usual, but start again if the scores add up to less than
         75.
-      </p>
-    </div>
-  </>
+      </Button>
+    </ul>
+  </div>
 );
 
 export default PresetsModalInteriorView;
