aa3868e8d5cab0812c88ae9b54db6ebf5292c410
Daniel Gill <dwgill@outlook.com>
2018-04-25 07:28:03 -0400

    Fix bug in colville preset

--- src/actions/presetColville.js
+++ src/actions/presetColville.js
@@ -2,13 +2,13 @@ import { PRESET_COLVILLE } from './types';
 import replaceRequirements from './replaceRequirements';
 import setAttributeRollType from './setAttributeRollType';
 import setRollInOrder from './setRollInOrder';
-import { scoreReq } from '../util/requirements';
+import { netModReq } from '../util/requirements';
 
 const presetColville = () => dispatch => {
   dispatch({ type: PRESET_COLVILLE });
   dispatch(setRollInOrder(true));
   dispatch(setAttributeRollType('STANDARD'));
-  dispatch(replaceRequirements([scoreReq('AT_LEAST', 2, 'AT_LEAST', 15)]));
+  dispatch(replaceRequirements([netModReq('AT_LEAST', 2)]));
 };
 
 export default presetColville;
