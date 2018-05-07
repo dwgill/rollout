cfeefbc5c140ec4020df1e980a5f6a81c9e436d7
Daniel Gill <dwgill@outlook.com>
2018-05-06 20:50:11 -0400

    Show message instead of disabling rollout button

--- src/components/RolloutDisplay/RollButton/RollButtonView.js
+++ src/components/RolloutDisplay/RollButton/RollButtonView.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-const RollButtonView = ({
-  onRollout: handleRollout,
-  stale,
-  reqStaleTtoRoll,
-}) => (
+const RollButtonView = ({ onRollout: handleRollout, stale, reqStaleTtoRoll }) =>
+  reqStaleTtoRoll && !stale ? (
+    <NoButton />
+  ) : (
+    <Button onRollout={handleRollout} stale={stale} />
+  );
+
+const Button = ({ stale, onRollout: handleRollout }) => (
   <button
-    className={styles.btn}
+    className={stale ? styles.btnStale : styles.btnFresh}
     onClick={handleRollout}
-    disabled={reqStaleTtoRoll && !stale}
   >
     Rollout
   </button>
+);
+
+const NoButton = () => (
+  <div className={styles.noBtn}>
+    <p className={styles.noBtnText}>
+      No Arbitrary <br /> Rerolls!
+    </p>
+  </div>
 );
 
 export default RollButtonView;
